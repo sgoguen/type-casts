9ed8ecc392cae5fc1f9334aca658322acafa06e0
sgoguen <sgoguen@gmail.com>
2020-06-17 18:54:09 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component, VERSION, OnInit } from "@angular/core";
 })
 export class AppComponent {
   name = "Angular " + VERSION.major;
+
+  myCounter = counters();
+}
+
+function counters() {
+  const myCounter = {
+    counters: [1, 2, 3].map(x => makeCounter(x)),
+    addCounter() {
+      this.counters.push(makeCounter(0));
+    }
+  };
+
+  return myCounter;
+
+  function makeCounter(initialValue: number) {
+    return {
+      count: initialValue,
+      increment() {
+        this.count = this.count + 1;
+      },
+      deleteMe() {
+        const newCounters = myCounter.counters.filter(c => c !== this);
+        myCounter.counters = newCounters;
+      }
+    };
+  }
 }
